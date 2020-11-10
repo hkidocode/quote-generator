a8f25ef2768fbcc9ef3b406177b036efc037d66a
hkidocode <57941226+hkidocode@users.noreply.github.com>
2020-11-10 21:25:12 +0100

    fetching data and manipulating DOM

---
+++ main.js
@@ -0,0 +1,43 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const newQuoteBtn = document.querySelector('.new-quote');
+const twitterBtn = document.getElementById('twitter');
+
+// Get Quote From API
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If Author is blank, add 'Unknown'
+        if (data.quoteAuthor === "") {
+            quoteAuthor.innerText = 'Unknown';
+        } else {
+            quoteAuthor.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = quoteAuthor.innerText;
+    const tweetUrl = `https://twitter.com/intent/tweet?${quote} - ${author}`;
+    window.open(tweetUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click',getQuote);
+twitterBtn.addEventListener('click',tweetQuote);
